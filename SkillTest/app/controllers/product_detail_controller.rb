b7045244b8a3c37e3baa42812ee4013d81ded0fd
N.Whitely <nwhitely90@gmail.com>
2017-09-03 23:20:41 +0900

    Fix related products feature layout

--- SkillTest/app/controllers/product_detail_controller.rb
+++ SkillTest/app/controllers/product_detail_controller.rb
@@ -7,6 +7,6 @@ class ProductDetailController < FrontBaseController
     end
     
     @site = Site.find(@product.site_id)
-    @related_products = Product.tagged_with(@product.tag_list).where("products.id <> ?", @product.id).limit(5)
+    @related_products = Product.tagged_with(@product.tag_list).where.not(id: @product.id).limit(5)
   end
 end
